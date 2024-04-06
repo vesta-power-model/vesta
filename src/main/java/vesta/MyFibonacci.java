8de96d8ef98d17d61e567e8593446fca7183fc9a
vesta-power-model <vesta.power.model@gmail.com>
2024-04-06 11:32:37 -0600

    other minor fixes

--- src/main/java/vesta/MyFibonacci.java
+++ src/main/java/vesta/MyFibonacci.java
@@ -1,5 +1,8 @@
 package vesta;
 
+import java.time.Duration;
+import java.time.Instant;
+
 final class MyFibonacci {
   private static int fib(int n) {
     if (n == 0 || n == 1) {
@@ -13,9 +16,13 @@ final class MyFibonacci {
     int iterations = Integer.parseInt(args[0]);
     PowercapCollector collector = new PowercapCollector();
     for (int i = 0; i < iterations; i++) {
+      Instant start = Instant.now();
       collector.start();
       fib(42);
       collector.stop();
+      System.out.println(
+          String.format(
+              "computed fib(42) in %s millis", Duration.between(start, Instant.now()).toMillis()));
     }
     collector.dump();
   }
