895e0cef8c375a9822f2abccd722373c763a5a57
vesta-power-model <vesta.power.model@gmail.com>
2024-04-06 19:39:16 -0600

    fixing an error

--- src/main/java/vesta/SampleCollector.java
+++ src/main/java/vesta/SampleCollector.java
@@ -111,7 +111,7 @@ public final class SampleCollector {
   // TODO: do we want a different data type? what happens if we need more data like counters?
   public void dump() {
     executor.shutdown();
-    if (!samples.isEmpty()) {
+    if (samples.isEmpty()) {
       System.out.println("no energy samples collected!");
       return;
     }
